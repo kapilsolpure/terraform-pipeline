02128401aa95c81ab42b13b7d9abbbb47deae308
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-08 15:22:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
-variable "region" {
-  default = "ap-south-1"
+#variable "region" {
+  #default = "ap-south-1"
 }
 
-variable "instance_type" {
-  default = "t2.micro"
+#variable "instance_type" {
+  #default = "t2.micro"
 }
 
-variable "ami" {
-  description = "Amazon Linux 2 AMI"
-  default     = "ami-0861f4e788f5069dd"  # Update based on your region
+#variable "ami" {
+  #description = "Amazon Linux 2 AMI"
+  #default     = "ami-0861f4e788f5069dd"  # Update based on your region
 }
 
