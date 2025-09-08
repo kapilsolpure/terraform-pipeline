e976ad05fbc33a6339ceedd69a67b6512749f5ba
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-08 12:46:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,87 @@
+# Create S3 Bucket for storing Terraform state
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "kapil-terraformstatefile-bucket-12345678"  # Use a globally unique name
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "Terraform State"
+    Environment = "Dev"
+  }
+}
+
+# Get default VPC and subnet
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  # User data to install a web server and show "Welcome"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Welcome</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "TerraformWebServer"
+  }
+}
 
