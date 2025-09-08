b4cc1f35bf26f2e8890ab757397efaf422fe7ec2
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-08 14:15:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-# Create S3 Bucket for storing Terraform state
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "kapil-terraformstatefile-bucket-12345678"  # Use a globally unique name
-
+  bucket = "kapil-terraformstatefile-bucket-12345678"
+  
   versioning {
     enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
   }
 
   tags = {
@@ -20,57 +11,37 @@ resource "aws_s3_bucket" "tf_state" {
   }
 }
 
-# Get default VPC and subnet
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-# Security Group
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
+  # (your security group code remains the same)
 }
 
-# EC2 Instance
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-  # User data to install a web server and show "Welcome"
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
