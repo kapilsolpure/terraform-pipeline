bd1ee44c273309a4881fcbfb440d4c05d0b752cd
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 14:36:02 +0530

    Create s3_bucket.tf

---
+++ s3_bucket.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "kapil-terraformstatefile-bucket-1234567810"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "TerraformStateBucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
