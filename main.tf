3cc8db1f65a307defa67094cf34a333821c80152
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 16:00:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ data "aws_vpc" "default" {
 # Security Group for EC2 Instance
 # --------------------------------------
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
+  name        = "pipeline-sg"
   description = "Allow SSH and HTTP"
   vpc_id      = data.aws_vpc.default.id
 
