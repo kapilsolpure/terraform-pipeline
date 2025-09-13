c998c379ec8cda5cf97cffe9e1b6ef7ed143be50
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 14:40:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kapil-terraformstatefile-bucket-1234567810"
+    #bucket = "kapil-terraformstatefile-bucket-1234567810"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
