723246e8e24030eb072dac39868c70ff9f71eff9
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 16:08:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,5 @@
 variable "ami" {
   description = "AMI ID to use for the EC2 instance"
   type        = string
-  default   = "ami-01f2711b72e050dbe"  # optional, set your default AMI here
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+  default     = "ami-01f2711b72e050dbe"
 }
