355eadacba014d855938495f79365c07d26128d7
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 14:17:50 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
-  sensitive   = false
+  description = "Public IP of the EC2 instance"
 }
