5c6c187dd927259a7b67d99ee294d2d817a52e22
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 14:57:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "kapil-terraformstatefile-bucket-1234567810"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-  }
-}
+#terraform {
+  #backend "s3" {
+    #bucket = "kapil-terraformstatefile-bucket-1234567810"
+    #key    = "terraform.tfstate"
+    #region = "ap-south-1"
+    #encrypt = true
+  #}
+#}
 
