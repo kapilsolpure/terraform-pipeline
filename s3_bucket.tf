1d4f07cfc6ebfbc96333e45f795b6fec6d140b40
kapilsolpure <85092766+kapilsolpure@users.noreply.github.com>
2025-09-13 15:11:06 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -4,12 +4,12 @@
 
   #versioning {
     #enabled = true
-  }
+  #}
 
   #tags = {
    # Name = "TerraformStateBucket"
-  }
-}
+  #}
+#}
 
 #resource "aws_s3_bucket_public_access_block" "block" {
  # bucket = aws_s3_bucket.tf_state.id
@@ -18,4 +18,4 @@
   #block_public_policy     = true
   #ignore_public_acls      = true
   #restrict_public_buckets = true
-}
+#}
